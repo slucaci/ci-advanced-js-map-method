2b15b205c6b8fc3d9f5e8641c8420db73ddfd94c
slucaci <serjlucaci@gmail.com>
2024-09-10 19:57:37 +0100

    Initial commit

--- map.js
+++ map.js
@@ -1,39 +1,42 @@
-  
 /**
- * To run this file in Gitpod, use the 
+ * To run this file in Gitpod, use the
  * command node map.js in the terminal
  */
 
 // Using a for loop
 
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+
+console.log(restults);
 
 // Using map()
 
-
 // Simplified w/ map()
 
-
 // Simplfied w/ map() + arrow function
-
 
 // With objects:
 const students = [
   {
     id: 1,
-    name: 'Mark',
-    profession: 'Developer',
-    skill: 'JavaScript'
+    name: "Mark",
+    profession: "Developer",
+    skill: "JavaScript",
   },
   {
     id: 2,
-    name: 'Ariel',
-    profession: 'Developer',
-    skill: 'HTML'
+    name: "Ariel",
+    profession: "Developer",
+    skill: "HTML",
   },
   {
     id: 3,
-    name: 'Jason',
-    profession: 'Designer',
-    skill: 'CSS'
+    name: "Jason",
+    profession: "Designer",
+    skill: "CSS",
   },
 ];
